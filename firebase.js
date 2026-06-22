6ac1ab87021e99175f49471855c8952a239aebc5
anshusharma1-ui <anshusharna1@gmail.com>
2026-06-22 09:32:25 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDsrSrj2YuNQbKAfvZMhrmHf7RvZmzT9ds",
+  authDomain: "anshu-calendar-app.firebaseapp.com",
+  projectId: "anshu-calendar-app",
+  storageBucket: "anshu-calendar-app.firebasestorage.app",
+  messagingSenderId: "1053163969054",
+  appId: "1:1053163969054:web:b135845b6b4cc5d83dec0f",
+  measurementId: "G-6J1WG1605G"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+window.db = db;
+
+console.log("Firebase Connected");
