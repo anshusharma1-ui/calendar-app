f23da8f89afff37b4053f81b5ceb812d6c9e839e
anshusharma1-ui <anshusharna1@gmail.com>
2026-06-22 09:46:04 +0530

    Enhance firebase.js with Firestore readiness log
    Added console log for Firestore readiness.

--- firebase.js
+++ firebase.js
@@ -21,3 +21,26 @@ const db = getFirestore(app);
 window.db = db;
 
 console.log("Firebase Connected");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "...",
+  measurementId: "..."
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.db = db;
+
+console.log("Firebase Connected");
+console.log("Firestore Ready");
