44b79047ea6530a1eef5846549967a9b240d672d
anshusharma1-ui <anshusharna1@gmail.com>
2026-06-22 09:50:50 +0530

    Refactor firebase.js for cleaner initialization

--- firebase.js
+++ firebase.js
@@ -21,26 +21,4 @@ const db = getFirestore(app);
 window.db = db;
 
 console.log("Firebase Connected");
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-
-import {
-  getFirestore
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "...",
-  measurementId: "..."
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-window.db = db;
-
-console.log("Firebase Connected");
 console.log("Firestore Ready");
