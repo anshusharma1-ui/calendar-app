2a9c4fa08a74811dc08ee5f22729a7aaa9e5b744
anshusharma1-ui <anshusharna1@gmail.com>
2026-06-26 17:00:47 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,43 @@
-const CACHE_NAME = "calendar-app-v1";
+const CACHE_NAME = "calendar-app-v2";
 
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js"
-];
+self.addEventListener("install", (event) => {
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+    self.skipWaiting();
+
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
+self.addEventListener("activate", (event) => {
+
+    event.waitUntil(
+
+        caches.keys().then((cacheNames) => {
+
+            return Promise.all(
+
+                cacheNames.map((cache) => {
+
+                    if (cache !== CACHE_NAME) {
+
+                        return caches.delete(cache);
+
+                    }
+
+                })
+
+            );
+
+        }).then(() => self.clients.claim())
+
+    );
+
+});
+
+self.addEventListener("fetch", (event) => {
+
+    event.respondWith(
+
+        fetch(event.request).catch(() => caches.match(event.request))
+
+    );
+
 });
